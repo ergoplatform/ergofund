342caa4f5a86f196a5cb03c455a587072ed09d13
Ezio <ezi0.audit0re@protonmail.com>
2021-11-20 14:07:00 +0330

    added getting controlbox from scanner

--- src/config.js
+++ src/config.js
@@ -4,4 +4,8 @@ var config = {}
 config.explorer = {}
 config.explorer.baseUri = 'https://api.ergoplatform.com'
 
+// Scanner configs
+config.scanner = {}
+config.scanner.baseUri = 'http://127.0.0.1:9000'
+
 module.exports = config
--- src/jobs/networkScanner.js
+++ src/jobs/networkScanner.js
@@ -1,1 +1,44 @@
-console.log('hiiii')
+import axios from "axios";
+import config, { scanner } from "../config";
+
+class Scanner {
+
+    constructor(uri) {
+        this.backend = axios.create({
+            baseURL: uri,
+            timeout: 5000,
+            headers: {"Content-Type": "applications/json"}
+        })
+
+        this.controlBoxScanId = 1;  // TODO: read this id from storage
+    }
+
+    async registerControlBox() {
+        return this.backend.post(
+            '/scan/register', {
+                'scanName': 'ControlBox NFT',
+                'trackingRule': {
+                    'predicate': 'containsAsset',
+                    'assetId': '72c3fbce3243d491d81eb564cdab1662b1f8d4c7e312b88870cec79b7cfd4321'
+                }
+            }
+        ).then(res => res.data)  // TODO: save the scanId in storage
+    }
+
+    async getControlBox() {
+        return this.backend.get(
+            `/scan/unspentBoxes/${this.controlBoxScanId}`
+        ).then(res => res.data)
+    }
+}
+
+// TODO: register campaign boxes in scanner if it is not already registered
+// TODO: get campaigns from scanner
+// TODO: store campaigns in the database
+
+// FOR TEST PURPOSE:
+(async() => {
+    var scanner = new Scanner(config.scanner.baseUri);
+    const controlBox = await scanner.getControlBox();
+    console.log(controlBox);
+})();
